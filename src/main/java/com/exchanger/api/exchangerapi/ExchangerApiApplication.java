a522206c55f52cb583a0a921a80ede1ba7a52c47
Jorge Gonzalez <jgonzalezso@exchanger.com.pe>
2022-03-20 10:37:58 -0500

    Primer commit

---
+++ src/main/java/com/exchanger/api/exchangerapi/ExchangerApiApplication.java
@@ -0,0 +1,12 @@
+package com.exchanger.api.exchangerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangerApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/exchanger/api/exchangerapi/ExchangerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchanger.api.exchangerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
